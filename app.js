69e019681411474605c9e78e80cb4b8ad841cfdc
Vivek <vivekkumarsingh2002@gmail.com>
2022-01-30 01:01:29 +0530

    CORS error fix

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const feedRoutes = require('./routes/feed')
 
 app.use(bodyParser.json());
 
+app.use((req,res,next) =>{
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Method', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 
 app.use('/feed', feedRoutes);
 
