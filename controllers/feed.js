b0492a2c2a8fb7abb62f26e0557f155f9bac6873
Vivek <vivekkumarsingh2002@gmail.com>
2022-01-30 02:13:07 +0530

    validation

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,4 @@
+const { validationResult } = require('express-validator/check')
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
@@ -16,9 +17,13 @@ exports.getPosts = (req, res, next) => {
 };
 
 exports.createPost = (req, res, next) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(422).json({message: "Validation error", error: errors.array});
+    }
   const title = req.body.title;
   const content = req.body.content;
-
+  
   res.json({
     message: "Post Create Succesfully",
     post: {
--- routes/feed.js
+++ routes/feed.js
@@ -1,11 +1,15 @@
-const express = require('express');
-
-const feedController = require('../controllers/feed');
+const express = require("express");
+const { body } = require("express-validator/check");
+const feedController = require("../controllers/feed");
 const router = express.Router();
 
-router.get('/posts', feedController.getPosts);
-router.post('/createPost', feedController.createPost);
-
-
+router.get("/posts", feedController.getPosts);
+router.post("/createPost",
+  [
+    body('title').trim().isLength({ min: 5 }),
+    body('content').trim().isLength({ min: 5 }),
+  ],
+  feedController.createPost
+);
 
 module.exports = router;
