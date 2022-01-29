d925f9dbd54cfdd532bda1baed2eaa746240b6b2
Vivek <vivekkumarsingh2002@gmail.com>
2022-01-30 02:42:52 +0530

    mongoose connection

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-const bodyParser = require('body-parser');
-const feedRoutes = require('./routes/feed')
+const feedRoutes = require("./routes/feed");
 
 app.use(bodyParser.json());
 
-app.use((req,res,next) =>{
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Method', 'GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Method", "GET, POST, PUT, PATCH, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
 });
 
-app.use('/feed', feedRoutes);
+app.use("/feed", feedRoutes);
 
-app.listen(8080);
+mongoose
+  .connect(
+    "mongodb+srv://new-user-01:Jaimatadi@cluster0.3optd.mongodb.net/messages?retryWrites=true&w=majority"
+  )
+  .then((result) => {
+      console.log("Connected");
+    app.listen(8080);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
