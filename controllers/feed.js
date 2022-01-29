e4f9b37e8d7b2d5526d3eefe1c291fe849c60ebe
Vivek <vivekkumarsingh2002@gmail.com>
2022-01-30 01:57:12 +0530

    createPosts

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,24 +1,34 @@
-exports.getPosts = (req,res,next) =>{
-
-    res.status(200).json({
-        title : "Hello Peopel wa",
-        content: "Aur bhai kya chalr ha ?"
-    });
-
+exports.getPosts = (req, res, next) => {
+  res.status(200).json({
+    posts: [
+      {
+        _id: '1',
+        title: "Hello Phela Phela Post",
+        content: "Aur bhai kya chalr ha ?",
+        imageUrl: "images/dashimage.png",
+        creator : {
+            name: "Rnapdm"
+        },
+        createdAt: new Date()
+      },
+    ],
+  });
 };
 
-exports.createPost = (req,res,next) => {
+exports.createPost = (req, res, next) => {
+  const title = req.body.title;
+  const content = req.body.content;
 
-    const title = req.body.title;
-    const content = req.body.content;
-
-    res.json({
-        message: "Post Create Succesfully",
-        post: {
-            postId : new Date().toISOString(),
-            title: title,
-            content: content,
-        }
-    })
-
-}
+  res.json({
+    message: "Post Create Succesfully",
+    post: {
+      _id : new Date().toISOString(),
+      title: title,
+      content: content,
+      creator : {
+        name: "Rnapdm"
+    },
+    createdAt: new Date()
+    },
+  });
+};
